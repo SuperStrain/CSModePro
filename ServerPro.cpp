ef06e4213fa8c339e9417dbdfd29aabd67ba321b
SuperStrain <1143124497@qq.com>
2022-08-23 23:46:10 +0800

    服务端基本完成

--- ServerPro.cpp
+++ ServerPro.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include<string>
 #include<WinSock2.h>
-#pragma commcnt(lib,"ws2 32.lib")
+#pragma comment(lib,"ws2 32.lib")
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -67,8 +67,37 @@ int main(int argc, char *argv[])
 		{
 			flags = 0;
 			cout << "accept error.\n";
+			closesocket(serverSocket);
+			closesocket(clientSocket);
+			WSACleanup();
+			return 0;
 		}
+
+		if (!flags)
+		{
+			cout << "服务器收到一个客户端连接：" << inet_ntoa(client_sin.sin_addr) << endl;
+			flags = 1;
+		}
+
+		//接受数据信息
+		char rMsg[512];
+		memset(rMsg, 0, 512);//初始化rMgs，防止数组越界
+		int recvMsgLen = recv(clientSocket, rMsg, sizeof(rMsg), 0);
+		if (recvMsgLen)
+		{
+			cout << "\n已收到客户端消息：" << rMsg << endl;
+		}
+		string sendMsg;
+		cout << "\n请输入要发送的消息：" << endl;
+		//这里采用getline作为输入函数，因为cin函数不能输入空格（输入空格视为终止）
+		getline(cin, sendMsg);
+		send(clientSocket, sendMsg.c_str(), strlen(sendMsg.c_str()), 0);//发送给客户端
+
+		closesocket(clientSocket);//关闭客户端套接字
+
 	}
+	closesocket(serverSocket);//关闭服务器套接字
+	WSACleanup();
 	return 0;
 }
 
